c13c16e0ad73edc5ff020de6ac647fa2d62835ec
Daniel Johnson <dajohnson1978@gmail.com>
2016-01-05 08:59:04 -0600

    adding test case

--- web/ex5/factors/factor_test.go
+++ web/ex5/factors/factor_test.go
@@ -1,10 +1,13 @@
 package factors_test
 
 import (
-"."
-"testing"
+  "."
+  "testing"
 )
 
 func Test7(t *testing.T) {
-
+  list := factors.Factor(7)
+  if len(list) != 0 {
+    t.Fatal("7 should have no factors")
+  }
 }
