2a6f8caab54c379fa13364b730f1558302bd732c
Daniel Johnson <daniel.johnson@surescripts.com>
2016-01-05 10:11:11 -0600

    updating testing

--- web/ex5/factors/factor_test.go
+++ web/ex5/factors/factor_test.go
@@ -1,13 +1,30 @@
 package factors_test
 
 import (
-  "."
-  "testing"
+	"."
+	"testing"
 )
 
 func Test7(t *testing.T) {
-  list := factors.Factor(7)
-  if len(list) != 0 {
-    t.Fatal("7 should have no factors")
-  }
+	list := factors.Factor(7)
+	if len(list) != 0 {
+		t.Fatal("7 should have no factors")
+	}
+}
+
+func Test9(t *testing.T) {
+	list := factors.Factor(9)
+	if len(list) != 1 {
+		t.Fatal("9 should have one factor")
+	}
+	if list[0] != 3 {
+		t.Fatal("9 should have one factor: 3")
+	}
+}
+
+func Test11(t *testing.T) {
+	list := factors.Factor(10)
+	if len(list) != 0 {
+		t.Fatal("11 should have no factors")
+	}
 }
